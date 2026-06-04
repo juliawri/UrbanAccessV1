b478b5bf38cef34606543ab39f75fbbd47740946
Julia Wright <90080493+juliawri@users.noreply.github.com>
2026-06-04 14:19:09 -0400

    added longitude and latitude

--- routing.py
+++ routing.py
@@ -1,8 +1,38 @@
 import requests
 import json
+import polyline
+import math
 
 OTP_BASE_URL = "http://localhost:8080/otp/routers/default/plan"
 
+def haversine(lat1, lon1, lat2, lon2):
+    R = 6371000  # meters
+    phi1, phi2 = math.radians(lat1), math.radians(lat2)
+    dphi = math.radians(lat2 - lat1)
+    dlambda = math.radians(lon2 - lon1)
+
+    a = math.sin(dphi/2)**2 + math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
+    return 2 * R * math.asin(math.sqrt(a))
+
+def sample_along_line(coords, step=50):
+    if len(coords) < 2:
+        return coords
+
+    sampled = [coords[0]]
+    acc_dist = 0
+
+    for i in range(1, len(coords)):
+        lat1, lon1 = coords[i - 1]
+        lat2, lon2 = coords[i]
+
+        seg_dist = haversine(lat1, lon1, lat2, lon2)
+        acc_dist += seg_dist
+
+        if acc_dist >= step:
+            sampled.append((lat2, lon2))
+            acc_dist = 0
+
+    return sampled
 
 def get_routes(from_lat, from_lon, to_lat, to_lon,
                modes="WALK,TRANSIT",
@@ -36,7 +66,8 @@ def get_routes(from_lat, from_lon, to_lat, to_lon,
         }
 
         for leg in itin.get("legs", []):
-            route["legs"].append({
+
+            leg_data = {
                 "mode": leg.get("mode"),
                 "from": leg.get("from", {}).get("name"),
                 "to": leg.get("to", {}).get("name"),
@@ -44,8 +75,20 @@ def get_routes(from_lat, from_lon, to_lat, to_lon,
                 "end_time": leg.get("endTime"),
                 "duration_sec": leg.get("duration", 0) / 1000,
                 "distance_m": leg.get("distance", 0),
-                "route": leg.get("route")
-            })
+                "route": leg.get("route"),
+                "geometry_sampled_50m": None
+            }
+
+            # ONLY for walking legs
+            if leg.get("mode") == "WALK":
+                geom = leg.get("legGeometry", {}).get("points")
+
+                if geom:
+                    coords = polyline.decode(geom)  # [(lat, lon), ...]
+                    sampled = sample_along_line(coords, step=20)
+                    leg_data["geometry_sampled_50m"] = sampled
+
+            route["legs"].append(leg_data)
 
         routes_output.append(route)
 
@@ -62,8 +105,8 @@ def save_routes_to_json(routes, filename="otp_routes.json"):
 """exmample in montreal"""
 if __name__ == "__main__":
     routes = get_routes(
-        from_lat=45.5017, from_lon=-73.5673,   # downtown Montreal
-        to_lat=45.5590, to_lon=-73.6692        # Jarry Park area
+        from_lat=45.53080483132569, from_lon=-73.61353945157414,   # mila
+        to_lat=45.49758656380192, to_lon=-73.5799738767698        # accio cup
     )
 
     save_routes_to_json(routes, "montreal_routes.json")
